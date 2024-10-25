21ba61f6468251670d6fb1360703c00140f66533
Gabriel Goldstein <gabriel_gbarros@hotmail.com>
2024-10-25 10:20:11 -0300

    Initial commmit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  width: 100%;
+  height: 75px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    background-color: #555;
+  }
+`;
